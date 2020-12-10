e1740205e731495fe846936335c368b97ca5d8b3
Aida Piccato <aidapiccato@Aidas-MacBook-Pro.local>
2020-12-10 17:19:41 -0500

    final

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from utils import *
 
 if __name__ == '__main__':
     os.makedirs('data', exist_ok=True)
-    sim = Simulation(T=900, N_sensory_nets=8, N_sensory=512, N_rand=1024, amp_ext=1500, amp_ext_nonspecific=20,
-                     tau_d=200, tau_f=2000, ux_mod=40, u_init=0.2)
+    sim = Simulation(T=900, N_sensory_nets=8, N_sensory=512, N_rand=1024, amp_ext=1500, amp_ext_nonspecific=40,
+                     tau_d=200, gamma=0.35, beta=400, tau_f=2000, ux_mod=4, u_init=0.2)
     sim.reset(mus=[256])
     save(simulation=sim, run_results={})
     run_results = sim.run()
